3215367b1f940f64b5effb9a316c65cee31c6200
ballanse <ballanse>
2024-04-25 14:04:37 +0000

    Pushed from Locofy Builder

---
+++ pages/index.tsx
@@ -0,0 +1,276 @@
+import type { NextPage } from "next";
+import styles from "./index.module.css";
+
+const NFTHomePageRevisionLandin: NextPage = () => {
+  return (
+    <div className={styles.nftHomePageRevisionLandin}>
+      <div className={styles.nftHomePageRevisionLandinInner}>
+        <div className={styles.lucidia2024AllRightsReseParent}>
+          <div className={styles.lucidia2024}>
+            © Lucidia 2024 | All rights reserved.
+          </div>
+          <div className={styles.yourJurisdictionMayContainer}>
+            <p className={styles.yourJurisdictionMay}>
+              YOUR JURISDICTION MAY NOT REGULATE CRYPTOCURRENCIES. THE VALUE OF
+              CRYPTOCURRENCIES MAY RISE AND FALL. PROFITS MAY BE SUBJECT TO
+              CAPITAL GAINS TAXES OR OTHER LOCAL TAXES.
+            </p>
+          </div>
+          <img className={styles.subtractIcon} alt="" src="/subtract.svg" />
+          <img className={styles.vectorParent} alt="" src="/subtract.svg" />
+          <div className={styles.groupChild} />
+          <img
+            className={styles.icons8SortUp2}
+            alt=""
+            src="/icons8sortup-2.svg"
+          />
+          <div className={styles.joinTheCommunity}>
+            <b className={styles.joinTheCommunityContainer}>
+              <p className={styles.yourJurisdictionMay}>join the community</p>
+            </b>
+            <img
+              className={styles.joinTheCommunityChild}
+              alt=""
+              src="/group-624827.svg"
+            />
+            <img
+              className={styles.joinTheCommunityItem}
+              alt=""
+              src="/group-624827.svg"
+            />
+            <img
+              className={styles.joinTheCommunityInner}
+              alt=""
+              src="/group-624827.svg"
+            />
+            <div className={styles.fullName}>Full name</div>
+            <div className={styles.eMail}>E-mail</div>
+            <div className={styles.diskord}>Diskord</div>
+            <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+            <b className={styles.subscribe}>subscribe</b>
+          </div>
+          <div className={styles.general}>
+            <div className={styles.generalChild} />
+            <div className={styles.generalItem} />
+            <div className={styles.privacyPolicyTermsAndContainer}>
+              <p className={styles.privacyPolicy}>PRIVACY POLICY</p>
+              <p className={styles.yourJurisdictionMay}>TERMS AND CONDITIONS</p>
+              <p className={styles.yourJurisdictionMay}>
+                TOKEN SALE TERMS AND CONDITIONS
+              </p>
+            </div>
+            <div className={styles.contactUsFrequentlyAskedContainer}>
+              <p className={styles.yourJurisdictionMay}>CONTACT US</p>
+              <p className={styles.yourJurisdictionMay}>
+                FREQUENTLY ASKED QUESTIONS
+              </p>
+              <p className={styles.yourJurisdictionMay}>
+                ANTI-MONEY LAUNDERING POLICY
+              </p>
+            </div>
+            <b className={styles.general1}>GENERAL</b>
+            <b className={styles.new}>NEW</b>
+            <b className={styles.new1}>NEW</b>
+            <b className={styles.company}>COMPANY</b>
+          </div>
+        </div>
+      </div>
+      <div className={styles.highresscreenshot000021Parent}>
+        <img
+          className={styles.highresscreenshot000021Icon}
+          alt=""
+          src="/highresscreenshot00002-1@2x.png"
+        />
+        <div className={styles.frameChild} />
+      </div>
+      <div className={styles.groupParent}>
+        <div className={styles.vectorParent}>
+          <img className={styles.vectorIcon1} alt="" src="/vector1.svg" />
+          <div className={styles.vectorGroup}>
+            <img className={styles.vectorIcon2} alt="" src="/vector2.svg" />
+            <b className={styles.connectWallet}>Connect wallet</b>
+          </div>
+          <img className={styles.subtractIcon2} alt="" src="/subtract1.svg" />
+        </div>
+        <div className={styles.gamesParent}>
+          <div className={styles.games}>GAMES</div>
+          <div className={styles.games}>TURNAMENTS</div>
+          <div className={styles.games}>MARKET PLACE</div>
+          <div className={styles.games}>DAO</div>
+          <div className={styles.games}>SUPPORT HUB</div>
+          <div className={styles.aboutParent}>
+            <div className={styles.about}>ABOUT</div>
+            <img className={styles.groupIcon} alt="" src="/group@2x.png" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.vectorContainer}>
+        <img className={styles.vectorIcon3} alt="" src="/vector3.svg" />
+        <b className={styles.joinNow}>JOIN now</b>
+      </div>
+      <div className={styles.frameParent}>
+        <div className={styles.timeParent}>
+          <div className={styles.time}>
+            <div className={styles.hour}>
+              <div className={styles.hourChild} />
+              <b className={styles.b}>00</b>
+            </div>
+            <b className={styles.b1}>:</b>
+            <b className={styles.b2}>:</b>
+            <div className={styles.hour1}>
+              <div className={styles.hourChild} />
+              <b className={styles.b3}>00</b>
+            </div>
+            <div className={styles.hour2}>
+              <div className={styles.hourChild} />
+              <b className={styles.b3}>00</b>
+            </div>
+          </div>
+          <div className={styles.hourParent}>
+            <b className={styles.hour3}>Hour</b>
+            <b className={styles.minute}>Minute</b>
+            <b className={styles.second}>Second</b>
+          </div>
+        </div>
+        <div className={styles.frameItem} />
+        <div className={styles.worthOfNftInTotalParent}>
+          <b className={styles.worthOfNftContainer}>
+            <span>$20,000</span>
+            <span className={styles.worthOfNft}> worth of NFT in Total</span>
+          </b>
+          <b className={styles.airdrop}>Airdrop</b>
+        </div>
+      </div>
+      <img
+        className={styles.nftHomePageRevisionLandinChild}
+        alt=""
+        src="/group-625023@2x.png"
+      />
+      <img className={styles.maskGroupIcon} alt="" src="/mask-group@2x.png" />
+      <div className={styles.ctaParent}>
+        <img className={styles.ctaIcon} alt="" src="/cta.svg" />
+        <div className={styles.teamMemberParent}>
+          <div className={styles.teamMember}>TEAM member</div>
+          <div className={styles.teamMember}>CAREER</div>
+          <div className={styles.roadmap}>roadmap</div>
+          <div className={styles.frameInner} />
+          <div className={styles.academy}>ACADEMY</div>
+          <div className={styles.frameInner} />
+          <div className={styles.teamMember}>FAQ</div>
+        </div>
+      </div>
+      <div className={styles.nftHomePageRevisionLandinItem} />
+      <div className={styles.maskGroupParent}>
+        <img className={styles.maskGroupIcon1} alt="" src="/mask-group.svg" />
+        <div className={styles.curseOfThe}>
+          Curse Of The Pharaoh is an action/adventure/survival game set in a
+          post-apocalyptic world overrun by zombies. Collect weapon, bullet, and
+          fuel props. Travel through open-world environment with different
+          biomes.
+        </div>
+      </div>
+      <img className={styles.image57Icon} alt="" src="/image-57@2x.png" />
+      <img className={styles.image56Icon} alt="" src="/image-56@2x.png" />
+      <div className={styles.ellipseDiv} />
+      <div className={styles.nftHomePageRevisionLandinChild1} />
+      <div className={styles.nftHomePageRevisionLandinChild2} />
+      <div className={styles.nftHomePageRevisionLandinChild3} />
+      <div className={styles.benefitsYouCan}>
+        Benefits you can enjoy from lucidia passport
+      </div>
+      <div className={styles.frameGroup}>
+        <div className={styles.rectangleParent}>
+          <div className={styles.rectangleDiv} />
+          <div className={styles.guaranteedTokenAirdrop}>
+            Guaranteed token airdrop whitelist for wallet holding NFT.
+          </div>
+          <div className={styles.div}>1</div>
+        </div>
+        <div className={styles.rectangleParent}>
+          <div className={styles.rectangleDiv} />
+          <div className={styles.earlyAccessTo}>
+            Early access to certain areas of the Lucidia ecosystem.
+          </div>
+          <div className={styles.div1}>2</div>
+        </div>
+        <div className={styles.rectangleParent}>
+          <div className={styles.rectangleDiv} />
+          <div className={styles.specialDiscordBadge}>
+            Special Discord badge “Early Supporter”.
+          </div>
+          <div className={styles.div1}>3</div>
+        </div>
+      </div>
+      <div className={styles.frameContainer}>
+        <div className={styles.rectangleParent}>
+          <div className={styles.rectangleDiv} />
+          <div className={styles.specialAccessSecret}>
+            Special access secret dev channel at Discord which will allow you to
+            speak directly with the Lucidia devs.
+          </div>
+          <div className={styles.div1}>4</div>
+        </div>
+        <div className={styles.rectangleParent1}>
+          <div className={styles.rectangleDiv} />
+          <div className={styles.offForFirst}>
+            50% off for first t-shirt of Lucidia merch.
+          </div>
+          <div className={styles.div1}>5</div>
+        </div>
+        <div className={styles.rectangleParent2}>
+          <div className={styles.rectangleDiv} />
+          <div className={styles.exclusiveAccessTo}>
+            Exclusive access to be first users of Lucidia DAO platform.
+          </div>
+          <div className={styles.div1}>6</div>
+        </div>
+      </div>
+      <img
+        className={styles.shutterstock18688248614Icon}
+        alt=""
+        src="/shutterstock-1868824861-4@2x.png"
+      />
+      <img
+        className={styles.shutterstock18688248613Icon}
+        alt=""
+        src="/shutterstock-1868824861-3@2x.png"
+      />
+      <img
+        className={styles.shutterstock18688248615Icon}
+        alt=""
+        src="/shutterstock-1868824861-5@2x.png"
+      />
+      <div className={styles.rectangleParent3}>
+        <div className={styles.frameChild6} />
+        <div className={styles.frameParent1}>
+          <div className={styles.easyStepParent}>
+            <b className={styles.easyStep}>EASY STEP</b>
+            <b className={styles.howToGet}>HOW TO GET THE AIRDROP</b>
+          </div>
+          <div className={styles.frameParent2}>
+            <div className={styles.parent}>
+              <b className={styles.b5}>1</b>
+              <div className={styles.connectYourWallet}>
+                CONNECT YOUR WALLET
+              </div>
+            </div>
+            <div className={styles.group}>
+              <b className={styles.b5}>2</b>
+              <div className={styles.joinWithOur}>JOIN WITH OUR COMMUNITY</div>
+            </div>
+            <div className={styles.group}>
+              <b className={styles.b5}>3</b>
+              <div className={styles.joinWithOur}>COMPLETE ASSESMENT</div>
+            </div>
+            <div className={styles.group}>
+              <b className={styles.b8}>4</b>
+              <div className={styles.joinWithOur}>RECEIVE YOUR REWARDS</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NFTHomePageRevisionLandin;
